1018e92d806e66baa6cd728f24a4a29a7d7b6a3f
Nguyen Tuan Dat <83784299+luzinguyen@users.noreply.github.com>
2022-11-15 13:41:45 +0900

    Update ketqua.js

--- js/ketqua.js
+++ js/ketqua.js
@@ -4,7 +4,7 @@ function ketqua() {
         document.getElementById("nhapsobaodanh").innerHTML = "受験番号を入力してください。";
         document.getElementById("img_ketqua").src = "";
     } else if (number == 20086) {
-        document.getElementById("nhapsobaodanh").innerHTML = "DM mày vào trường là mày ngu";
+        document.getElementById("nhapsobaodanh").innerHTML = "合格、おめでとうございます。";
         document.getElementById("img_ketqua").src = "../img/gokaku.png";
     } else {
         document.getElementById("nhapsobaodanh").innerHTML = "残念、不合格です。";
